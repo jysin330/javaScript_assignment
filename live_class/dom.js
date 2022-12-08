106955325b71f5f06a725ddb6bc2b6701fcdd30a
Jyoti Singh <jyotisingh82000@gmail.com>
2022-12-08 12:29:28 +0530

    delete some files

--- live_class/dom.js
+++
@@ -1,1 +0,0 @@
-// dom represent document object model. dom represent the page so that the program(js) can change the document , structure, style and content
--- recorded_cls.js/function.js
+++
@@ -1,5 +0,0 @@
-function namstey() {
-    return "hello india";
-}
-var greeting = namstey();
-console.log(greeting);
